bbd329d91d23cdf3d48b0af4f61d6d873ec09b6d
Muhammad Baziq Khan <baziq12@gmail.com>
2025-06-21 22:10:42 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine Learning Project to Handle Uber Demand',
+    author='Baziq Khan',
+    license='',
+)
